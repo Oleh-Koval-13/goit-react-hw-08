c18b7a399d988415e06cc8f2fdac1e5bd7fee0ca
Oleh-Koval-13 <solder363191999@gmail.com>
2024-04-12 17:27:26 +0300

    update

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,5 +1,20 @@
-export const selectLoading = state => state.contacts.loading;
+import { createSelector } from "@reduxjs/toolkit";
 
-export const selectError = state => state.contacts.error;
+export const selectContacts = (state) => state.contacts.items;
 
-export const selectContacts = state => state.contacts.items;
+export const selectLoading = (state) => state.contacts.loading;
+
+export const selectError = (state) => state.contacts.error;
+
+export const selectNameFilter = (state) => state.filters.name;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectNameFilter],
+  (contacts, nameFilter) => {
+    return contacts.filter(
+      (contact) =>
+        contact.name.toLowerCase().includes(nameFilter.toLowerCase()) ||
+        contact.number.includes(nameFilter)
+    );
+  }
+);
